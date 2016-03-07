5f569cfca6e3061ce7f4c8e4a9ef0ceff9a9f56b
AJP <allenatmc@gmail.com>
2016-03-07 10:17:25 -0600

    add timeout to navbar display

--- scripts/mainCtrl.js
+++ scripts/mainCtrl.js
@@ -1,9 +1,13 @@
-app.controller('MainCtrl', function($scope, $location, $anchorScroll) {
+app.controller('MainCtrl', function($scope, $location, $anchorScroll, $timeout) {
+  var that = this;
 
   this.toggleClass = function(elementId, className){
     angular.element('#' + elementId).toggleClass(className);
   };
 
+  $timeout(function(){
+    that.toggleClass('sideNav', 'active')
+  }, 1000);
 
 
 });
