36333d85a3229eb0c797216ca7375388a849781c
AJP <allenatmc@gmail.com>
2016-03-10 00:42:38 -0600

    add education content

--- scripts/materializeScripts.js
+++ scripts/materializeScripts.js
@@ -5,6 +5,6 @@
 
     $(document).ready(function(){
     $('.collapsible').collapsible({
-      accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+      // accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
   });
